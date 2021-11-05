195bd3d0bf0da23403db99cf8544c46251ee0a5a
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-11-05 00:10:20 -0500

    Updated Readme and pep8 changes

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 """Method that determines if a box can be opened"""
 
+
 def canUnlockAll(boxes):
-	"""Determines if a box can be opened"""
-	keys = [0]
-	last = 0
-
-	while last < len(keys):
-		last = len(keys)
-		for box in range(len(boxes)):
-			if box in keys:
-				for key in boxes[box]:
-					if key < len(boxes) and key not in keys:
-						keys.append(key)
-
-	return len(keys) == len(boxes)
+    """Determines if a box can be opened"""
+    keyring = [0]
+    lastBox = 0
+    
+    while lastBox < len(keyring):
+        lastBox = len(keyring)
+        for box in range(len(boxes)):
+            if box in keyring:
+                for key in boxes[box]:
+                    if key < len(boxes) and key not in keyring:
+                        keyring.append(key)
+    return len(keyring) == len(boxes)
