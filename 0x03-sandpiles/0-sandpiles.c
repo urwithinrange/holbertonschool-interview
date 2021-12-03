935c439bb34401bfaf114c6bd8c3011e592c27db
Derek <69029867+urwithinrange@users.noreply.github.com>
2021-12-02 22:30:13 -0600

    Checking with betty litner

--- 0x03-sandpiles/0-sandpiles.c
+++ 0x03-sandpiles/0-sandpiles.c
@@ -12,50 +12,39 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 	int temp_grid[3][3];
 
 	for (i = 0; i < 3; i++)
-	        for (j = 0; j < 3; j++)
-                        grid1[i][j] += grid2[i][j];
+	    for (j = 0; j < 3; j++)
+            grid1[i][j] += grid2[i][j];
         while (!stabilized(grid1))
         {
 	        print_grid(grid1);
 
-		for (i = 0; i < 3; i++)
+			for (i = 0; i < 3; i++)
 		        for (j = 0; j < 3; j++)
 			        temp_grid[i][j] = 0;
 
-		for (i = 0; i < 3; i++)
-		{
-			for (j = 0; j < 3; j++)
-			{
-				if (j != 0 && grid1[i][j - 1] > 3)
+			for (i = 0; i < 3; i++)
+				for (j = 0; j < 3; j++)
 				{
-					temp_grid[i][j]++;
+					if (j != 0 && grid1[i][j - 1] > 3)
+						temp_grid[i][j]++;
+					if (j != 2 && grid1[i][j + 1] > 3)
+						temp_grid[i][j]++;
+					if (i != 0 && grid1[i - 1][j] > 3)
+					  temp_grid[i][j]++;
+					if (i != 2 && grid1[i + 1][j] > 3)
+						temp_grid[i][j]++;
 				}
-				if (j != 2 && grid1[i][j + 1] > 3)
-				{
-					temp_grid[i][j]++;
-				}
-				if (i != 0 && grid1[i - 1][j] > 3)
-				{
-				  temp_grid[i][j]++;
-				}
-				if (i != 2 && grid1[i + 1][j] > 3)
-				{
-					temp_grid[i][j]++;
-				}
-			}
-		}
 
-		for (i = 0; i < 3; i++)
-		{
-			for (j = 0; j < 3; j++)
-			{
-				if (grid1[i][j] > 3)
-					grid1[i][j] -= 4;
-				grid1[i][j] += temp_grid[i][j];
-			}
+			for (i = 0; i < 3; i++)
+				for (j = 0; j < 3; j++)
+				{
+					if (grid1[i][j] > 3)
+						grid1[i][j] -= 4;
+					grid1[i][j] += temp_grid[i][j];
+				}
+
 		}
-	}
-}
+}	
 
 /**
  *print_grid - a function used to print a grid
