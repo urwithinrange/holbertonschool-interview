71e4aefa869f403a0ed817dcfc757de7487be800
Derek_Clemens <urwithinrange@yahoo.com>
2021-12-03 04:26:21 +0000

    transfering to vscode

--- 0x03-sandpiles/0-sandpiles.c
+++ 0x03-sandpiles/0-sandpiles.c
@@ -6,28 +6,22 @@
  *Return: A stable grid1
  */
 
-void sandpiles_sum(grid1[1][3]
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	int i, j;
 	int temp_grid[3][3];
 
 	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
-			grid1[i][j] += grid2[i][j];
-		}
-	}
-	while (!stabilized(grid1))
-	{
-		print_grid(grid1);
+	        for (j = 0; j < 3; j++)
+                        grid1[i][j] += grid2[i][j];
+        while (!stabilized(grid1))
+        {
+	        print_grid(grid1);
 
 		for (i = 0; i < 3; i++)
-		{
-			for (j = 0; j < 3; j++)
-			{
-				temp_grid[i][j] = 0;
-			}
+		        for (j = 0; j < 3; j++)
+			        temp_grid[i][j] = 0;
+
 		for (i = 0; i < 3; i++)
 		{
 			for (j = 0; j < 3; j++)
@@ -42,22 +36,33 @@ void sandpiles_sum(grid1[1][3]
 				}
 				if (i != 0 && grid1[i - 1][j] > 3)
 				{
-					temp_grid[i][j]++;
+				  temp_grid[i][j]++;
 				}
-				if (i != 2 && grid1[i + 1][j -3] > 3)
+				if (i != 2 && grid1[i + 1][j] > 3)
 				{
 					temp_grid[i][j]++;
 				}
 			}
 		}
+
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				if (grid1[i][j] > 3)
+					grid1[i][j] -= 4;
+				grid1[i][j] += temp_grid[i][j];
+			}
+		}
 	}
 }
+
 /**
  *print_grid - a function used to print a grid
- *@grid: grid
+ *@temp_grid: grid1 copy
  *Return: void
  */
-static void print_grid(int grid[3][3])
+static void print_grid(int temp_grid[3][3])
 {
 	int i, j;
 
@@ -66,16 +71,18 @@ static void print_grid(int grid[3][3])
 	{
 		for (j = 0; j < 3; j++)
 		{
-			printf("%d", grid[i][j]);
+			printf("%d", temp_grid[i][j]);
 		}
 		printf("\n");
 	}
+}
+
 /**
  *stabilized - a function used to check if a grid is stabilized
- *@grid: grid
+ *@temp_grid: grid1 copy
  *Return: 1 if stabilized, 0 if not
  */
-int stabilized(int grid[3][3])
+int stabilized(int temp_grid[3][3])
 {
 	int i, j;
 
@@ -83,7 +90,7 @@ int stabilized(int grid[3][3])
 	{
 		for (j = 0; j < 3; j++)
 		{
-			if (grid[i][j] > 3)
+			if (temp_grid[i][j] > 3)
 			{
 				return (0);
 			}
