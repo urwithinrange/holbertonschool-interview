9396ed710e33483c2f8a5d0b3752e84bf55dcb6f
Derek_Clemens <urwithinrange@yahoo.com>
2021-12-22 06:35:28 +0000

    Created a recursive function to alleviate the memory alloc issue

--- 0x04-linked_list_palindrome/0-is_palindrome.c
+++ 0x04-linked_list_palindrome/0-is_palindrome.c
@@ -5,44 +5,29 @@
  * @head: pointer to the head of the list
  * Return: 1 if it is a palindrome, 0 if it is not
  */
+
 int is_palindrome(listint_t **head)
 {
-listint_t *slow = *head;
-listint_t *fast = *head;
-listint_t *prev = NULL;
-listint_t *next = NULL;
-int i = 0;
-/* the list is empty or has only one node, it is a palindrome */
-if (!head || !(*head))
-return (1);
-/* searching for the middle of the list */
-while (fast && fast->next)
-{
-prev = slow;
-slow = slow->next;
-fast = fast->next->next;
+	/* the list is empty or has only one node, it is a palindrome */
+	if (!head || !(*head))
+		return (1);
+	return (palindrome_recur(head, *head));
 }
-/* the middle node is skipped if the list has an odd number of nodes */
-if (fast)
-slow = slow->next;
-/* reversing the second half of the list*/
-while (slow)
+/**
+ * palindrome_recur - recursivly checks for a palindrome list
+ * @head: pointer to the head of the list
+ * @cur_node: the position in the list that is being compared to head pos
+ * Return: 1 if it is a palindrome, 0 if it is not
+ */
+int palindrome_recur(listint_t **head, listint_t *cur_node)
 {
-next = slow->next;
-slow->next = prev;
-prev = slow;
-slow = next;
-i++;
-}
-/* Where the comparison from head to a reversed second half */
-while (prev && i > 0)
-{
-if ((*head)->n == prev->n)
-{
-*head = (*head)->next;
-prev = prev->next;
-}
-i--;
-}
-return (i == 0 ? 1 : 0);
+	if (!cur_node)
+	return (1);
+	/*Using recursion to avoid creating a second list to be freed*/
+	if (palindrome_recur(head, cur_node->next) && (*head)->n == cur_node->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
 }
