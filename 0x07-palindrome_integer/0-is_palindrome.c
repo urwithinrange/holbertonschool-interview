3e5001f1f0622cf671a6a5ae0f73c5138258e4c1
Derek <69029867+urwithinrange@users.noreply.github.com>
2022-01-06 18:59:57 -0600

    initial push

---
+++ 0x07-palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,24 @@
+#include "palindrome.h"
+
+/**
+ * is_palindrome - checks if a singly linked list is a palindrome
+ * @n: integer to check
+ * Return: 1 if it is a palindrome, 0 if it is not
+ */
+int is_palindrome(unsigned long int n)
+{
+        unsigned long int tmp = 0, n_copy;
+
+        if (n < 0 || (n % 10 == 0 && n != 0))
+                return (0);
+        n_copy = n;
+        while (n_copy > 0)
+        {
+                tmp = tmp * 10 + n_copy % 10;
+                n_copy /= 10;
+        }
+        if (tmp == n)
+                return (1);
+        else
+                return (0);
+}
