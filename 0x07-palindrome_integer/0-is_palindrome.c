06303e738423ee42bfa66806d8fa2875460ce87d
Derek <69029867+urwithinrange@users.noreply.github.com>
2022-01-06 19:06:09 -0600

    made a couple changes to edge case

--- 0x07-palindrome_integer/0-is_palindrome.c
+++ 0x07-palindrome_integer/0-is_palindrome.c
@@ -1,7 +1,7 @@
 #include "palindrome.h"
 
 /**
- * is_palindrome - checks if a singly linked list is a palindrome
+ * is_palindrome - checks if a unsigned long int is a palindrome
  * @n: integer to check
  * Return: 1 if it is a palindrome, 0 if it is not
  */
@@ -9,7 +9,7 @@ int is_palindrome(unsigned long int n)
 {
         unsigned long int tmp = 0, n_copy;
 
-        if (n < 0 || (n % 10 == 0 && n != 0))
+        if (n < 0 || n % 10 == 0)
                 return (0);
         n_copy = n;
         while (n_copy > 0)
