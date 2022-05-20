26998bf8b0b1fa671129c47ca2bfa05fb887fb20
Derek <69029867+urwithinrange@users.noreply.github.com>
2022-05-19 21:43:16 -0500

    Adding a prime filter

--- 0x1C-prime_game.py/0-prime_game.py
+++ 0x1C-prime_game.py/0-prime_game.py
@@ -7,6 +7,21 @@ multiples from the set. Returns the player with the most wins
 or none for a tie.
 """
 
+
+def isPrime(num):
+	"""
+	Return: a list of primes in range of num
+	"""
+	primes = []
+	Pfilter = [True] * (num + 1)
+	for i in range(2, num + 1):
+		if Pfilter[i]:
+			primes.append(i)
+			for j in range(i * i, num + 1, i):
+				Pfilter[j] = False
+	return primes
+
+
 def isWinner(x, nums):
 	"""where x is the number of rounds and nums is an array of n
 	Return: name of the player that won the most rounds
@@ -15,27 +30,22 @@ def isWinner(x, nums):
 	You cannot import any packages in this task
 	players are Maria Player 1 and Ben player 2
 	"""
-	P1Score = 0
-	P2Score = 0
-	nums = sorted(nums)
-	
-	while x > 0:
-		for i in range(len(nums)):
-			
-		if nums[0] % 2 == 0:
-			P1Score += 1
-			nums.pop(0)
-		else:
-			P2Score += 1
-			nums.pop(0)
-		x -= 1
-		# for num in range(len(nums)):
-		# 	if num <= 1:
 
-		# 	if num % 2 == 0:
-		# 		P1Score += 1
-		# 		nums.remove(num)
-		# 	elif num % 2 == 1:
-		# 		P2Score += 1
-		# 		nums.remove(num)
-		# if nums[0] % 2 == 0
+	if x is None or nums is None or x == 0 or nums == []:
+		return None
+
+	P1Score = 0 #maria
+	P2Score = 0 #ben
+	
+	for i in range(x):
+		primes = isPrime(nums[i])
+		if len(primes) % 2 != 0:  # if the number of primes is odd
+			P1Score += 1
+		else:  # if the number of primes is even
+			P2Score += 1
+	if P1Score > P2Score:
+		return "Maria"
+	elif P2Score > P1Score:
+		return "Ben"
+	else:
+		return None
